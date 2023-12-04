1e92730380edabb7ffbba910156c3db08b318396
Theo AKBAS <akbas.theo@protonmail.com>
2023-12-04 12:04:30 +0100

    Added lines to compute convolution

--- main.cu
+++ main.cu
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 #include <math.h>
+#include <getopt.h>
 
 void MatrixInit(float *M, int n, int p){
     int i, j;
@@ -36,6 +37,21 @@ void MatrixPrint(float *M, int n, int p){
         printf("\n");
     }
     printf("\n");
+};
+
+void MatrixPrint3D(float *M, int n, int p, int d){
+    int i, j, k;
+    for(i=0; i<d; i++){
+        printf("Matrix %d:\n", i+1);
+
+        for(j=0; j<n; j++){
+            for(k=0; k<p; k++){
+                printf("%f ", M[i*p*p + j*p + k]);
+            }
+            printf("\n");
+        }
+        printf("\n");
+    }
 };
 
 void MatrixAdd(float *M1, float *M2, float *Mout, int n, int p){
@@ -73,6 +89,7 @@ __global__ void cudaMatrixMult(float *M1, float *M2, float *Mout, int n){
         Mout[i*n+j] = sum;
     }
 };
+
 __global__ void cudaMatrixAdd(float *M1, float *M2, float *Mout, int n, int p){
 	int i = blockIdx.x * blockDim.x + threadIdx.x;
     int j = blockIdx.y * blockDim.y + threadIdx.y;
@@ -82,42 +99,53 @@ __global__ void cudaMatrixAdd(float *M1, float *M2, float *Mout, int n, int p){
     }
 };
 
-__global__ void cudaMatrixMult(float *M1, float *M2, float *Mout, int n){
+__global__ void cudaMatrixConvolve(float *raw, float *kernel, float *Mout, int n, int p, int kernelSize){
+    // kernel is of size 6x5x5
     int j = blockIdx.x * blockDim.x + threadIdx.x;
     int i = blockIdx.y * blockDim.y + threadIdx.y;
+    int k = blockIdx.z * blockDim.z + threadIdx.z;
     
-    if(i < n && j < n){
-        Mout[i*n+j] = 0;
-        float sum = 0;
-        for(int k=0; k<n; k++){
-            sum += M1[i*n+k] * M2[k*n+j];
-        }
-        Mout[i*n+j] = sum;
-    }
-};
+    if(i < n && j < p && k < kernelSize){
+        Mout[k*n*p + i*p + j] = 0;
 
-__global__ void cudaMatrixMult(float *M1, float *M2, float *Mout, int n){
-    int j = blockIdx.x * blockDim.x + threadIdx.x;
-    int i = blockIdx.y * blockDim.y + threadIdx.y;
-    
-    if(i < n && j < n){
-        Mout[i*n+j] = 0;
+        // compute the multiplication of the kernel and the input matrix
         float sum = 0;
-        for(int k=0; k<n; k++){
-            sum += M1[i*n+k] * M2[k*n+j];
+        for(int q=0; q<kernelSize; q++){
+            for(int l=0; l<kernelSize; l++){
+                for(int m=0; m<kernelSize; m++){
+                    sum += kernel[q*kernelSize*kernelSize + l*kernelSize + m] * raw[(i+q)*p*p + (j+l)*p + m];
+                }
+            }
         }
-        Mout[i*n+j] = sum;
+        Mout[k*n*p + i*p + j] = sum; // assign the result to the output matrix
     }
-};
+}
 
 int main(int argc, char *argv[]) {
     srand(time(NULL));
     int N, P;
+    int input[2] = {0, 0};
+    int kernel[3] = {0, 0, 0};
 
     if (argc != 3) {
         printf("Usage: %s <number of rows (N)> <number of columns (P)>\n", argv[0]);
         return 1;
     }
+
+    // int opt;
+    // while ((opt = getopt(argc, argv, "i:k:")) != -1) {
+    //     switch (opt) {
+    //         case 'i':
+    //             sscanf(optarg, "(%d, %d)", &input[0], &input[1]);
+    //             break;
+    //         case 'k':
+    //             sscanf(optarg, "(%d, %d, %d)", &kernel[0], &kernel[1], &kernel[2]);
+    //             break;
+    //         default:
+    //             fprintf(stderr, "Usage: %s -i (x,y) -k (x,y)\n", argv[0]);
+    //             exit(EXIT_FAILURE);
+    //     }
+    // }
 
     N = atoi(argv[1]);
     P = atoi(argv[2]);
@@ -125,9 +153,9 @@ int main(int argc, char *argv[]) {
     // Allocation
     /////////////////
     // clock_t start_time_CPU = clock(); // start time measure for CPU
-
+    int kernel_size = 5;
     float* raw_data     = (float*)malloc(N*P*sizeof(float));
-    float* C1_kernel    = (float*)malloc(6*5*5*sizeof(float));
+    float* C1_kernel    = (float*)malloc(6*kernel_size*kernel_size*sizeof(float));
     float* C1_data      = (float*)malloc(6*28*28*sizeof(float));
     float* S1_data      = (float*)malloc(6*14*14*sizeof(float));
     //////////////////
@@ -135,9 +163,7 @@ int main(int argc, char *argv[]) {
     // Init
     /////////////////
     MatrixInit(raw_data, N, P);
-    MatrixInit(C1_kernel, N, P);
-    float C1_data[6][28][28];
-    float S1_data[6][14][14];
+    MatrixInit3D(C1_kernel, 6, kernel_size, kernel_size);
 
     // Initialize C1_data and S1_data to zeros
     cudaMemset(C1_data, 0, sizeof(float) * 6 * 28 * 28);
@@ -146,7 +172,7 @@ int main(int argc, char *argv[]) {
 
     // GPU Functions
     /////////////////
-    clock_t start_time_GPU = clock(); // start time measure for GPU
+    // clock_t start_time_GPU = clock(); // start time measure for GPU
 
     float *d_raw, *d_kernel, *d_C1_data, *d_S1_data;
     cudaMalloc((void**)&d_raw, sizeof(float)*N*P);
@@ -154,22 +180,20 @@ int main(int argc, char *argv[]) {
     cudaMalloc((void**)&d_C1_data, sizeof(float)*6*28*28);
     cudaMalloc((void**)&d_S1_data, sizeof(float)*6*14*14);
 
-    cudaMemcpy(d_raw, raw_data, sizeof(float)*N*P, cudaMemcpyHostToDevice);        // RAM --> GPU
-    cudaMemcpy(d_kernel, C1_kernel, sizeof(float)*N*P, cudaMemcpyHostToDevice);        // RAM --> GPU
-    cudaMemcpy(d_C1_data, C1_data, sizeof(float)*N*P, cudaMemcpyHostToDevice);    // RAM --> GPU
+    cudaMemcpy(d_raw, raw_data, sizeof(float)*N*P, cudaMemcpyHostToDevice);         // RAM --> GPU
+    cudaMemcpy(d_kernel, C1_kernel, sizeof(float)*N*P, cudaMemcpyHostToDevice);     // RAM --> GPU
+    cudaMemcpy(d_C1_data, C1_data, sizeof(float)*N*P, cudaMemcpyHostToDevice);      // RAM --> GPU
 
-    dim3 dimGrid(ceil(N/32.0), ceil(P/32.0));
-    dim3 dimBlock(32, 32);
+    dim3 dimGrid(28,28);    // 28x28 blocks of 1 thread each (2D) SO 28x28 = 784 threads
+    dim3 dimBlock(6, 1, 1); // 6 blocks of 1 thread each (1D) SO 6 threads
 
-    cudaMatrixAdd<<<dimGrid, dimBlock>>>(d_M1, d_M2, d_Mout, N, P);
-    cudaMemcpy(Mout, d_Mout, sizeof(float)*N*P, cudaMemcpyDeviceToHost);    // GPU --> RAM
+    cudaMatrixConvolve<<<dimGrid, dimBlock>>>(d_raw, d_kernel, d_C1_data, N, P, kernel_size);
+    cudaMemcpy(C1_data, d_C1_data, sizeof(float)*6*28*28, cudaMemcpyDeviceToHost);      // GPU --> RAM
 
-    cudaFree(d_M1);
-    cudaFree(d_M2);
-    cudaFree(d_Mout);
-
-    clock_t end_time_GPU = clock(); // end time measure for GPU
-    double execution_time_GPU = ((double) (end_time_GPU - start_time_GPU)) / CLOCKS_PER_SEC;
+    cudaFree(d_raw);
+    cudaFree(d_kernel);
+    cudaFree(d_C1_data);
+    cudaFree(d_S1_data);
     /////////////////
 
 
@@ -177,9 +201,9 @@ int main(int argc, char *argv[]) {
     /////////////////
     // MatrixPrint(M1, N, P);
     // MatrixPrint(M2, N, P);
-    // MatrixPrint(Mout, N, P);
-    printf("Execution time CPU: %f\n", execution_time_CPU);
-    printf("Execution time GPU: %f\n", execution_time_GPU);
+    MatrixPrint3D(C1_data, N, P, 6);
+    // printf("Execution time CPU: %f\n", execution_time_CPU);
+    // printf("Execution time GPU: %f\n", execution_time_GPU);
     /////////////////
 
     return 0;
